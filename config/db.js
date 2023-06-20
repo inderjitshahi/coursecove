fd4e1774379332f6f01808c274155bba9a4b6353
Inderjit Shahi <inderjitshahi_mc20a7_13@dtu.ac.in>
2023-06-20 16:47:07 +0530

    fixed db connection

--- config/db.js
+++ config/db.js
@@ -1,6 +1,4 @@
 import mongoose from "mongoose";
-
-
 export const connectDB= async()=>{
     const connection=await mongoose.connect(process.env.MONGODB_URI,{
         useNewUrlParser:true,
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+import * as dotenv from 'dotenv';
+//Load Env variables
+dotenv.config();
+
 import express from 'express';
 import morgan from 'morgan';
-import * as dotenv from 'dotenv';
 import { connectDB } from './config/db.js'
 import errorHandler from './middlewares/error.js';
 import fileUpload from 'express-fileupload';
@@ -13,8 +16,7 @@ import xss from 'xss-clean';
 import rateLimit from 'express-rate-limit';
 import hpp from 'hpp';
 import cors from 'cors'
-//Load Env variables
-dotenv.config({ path: './config/config.env' });
+
 
 
 //Route Files, **add file extension
@@ -29,8 +31,7 @@ import reviewRouter from './routes/reviews.js'
 connectDB();
 
 //Port
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT;
 //Initialize the app
 const app = express();
 
@@ -61,9 +62,9 @@ app.use(helmet());
 app.use(xss());
 
 //Rate Limiting
-const limiter=rateLimit({
-    windowMs:10*60*1000,//10min
-    max:400
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,//10min
+    max: 400
 })
 app.use(limiter);
 
@@ -74,14 +75,14 @@ app.use(hpp());
 app.use(cors());
 
 // Set static folder
-app.use(express.static(path.join(dir_name,'public')));
+app.use(express.static(path.join(dir_name, 'public')));
 
 //Mount Routers------------------
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
-app.use('/api/v1/auth',authRouter);
-app.use('/api/v1/auth/users',usersRouter);
-app.use('/api/v1/reviews',reviewRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/auth/users', usersRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 
 //Error handler middleware, must be after Routers Mount,so that errors returned from routes can be handled
